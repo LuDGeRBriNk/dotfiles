72ea18084162c85e33d81e95c59220114b2bcbb5
Luís F. <luis.fernando.a.b@gmail.com>
2026-03-04 14:42:48 -0300

    Created script to automate virt-manager.

--- hyprland/scripts/v-man.sh
+++ hyprland/scripts/v-man.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # This script automates virt-manager configs.
-# It requires whitelisting these commands in the sudoers file!
+# It REQUIRES whitelisting these commands in the sudoers file!
 
 # Start required services
 echo "Starting libvirt daemon..."
-sudo systemctl start libvirtd
+sudo /usr/bin/systemctl start libvirtd
 
 echo "Starting default network..."
-sudo virsh net-start default
+sudo /usr/bin/virsh net-start default
 
 echo "Launching virt-manager..."
 # The script will pause on this line until you close the virt-manager window
@@ -18,9 +18,9 @@ echo "virt-manager closed. Cleaning up..."
 
 #Stop required services
 echo "Stopping default network..."
-sudo virsh net-destroy default
+sudo /usr/bin/virsh net-destroy default
 
 echo "Stopping libvirt daemon..."
-sudo systemctl stop libvirtd
+sudo /usr/bin/systemctl stop libvirtd
 
 echo "Done!"
